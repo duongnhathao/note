a5ef25dee03306d2f01c7b30385a27773f2e4386
duongnhathao <duongnhathao001@gmail.com>
2022-06-20 10:32:36 +0700

    add auto clone

--- project_perfex/setup.sh
+++ project_perfex/setup.sh
@@ -18,21 +18,50 @@ while [ -z "$git" ]; do
 done
 arr["git"]="$git"
 arr["path_name"]="$path_install"
-git clone "$git $path_install" || (
-  sleep 5
+
+echo "cloning project"
+{
+  if [ -z "$path_install" ]; then
+    git clone "$git" .
+  else
+    git clone "$git" "$path_install"
+  fi
+  #  clear && echo "clone success"
+} || {
+  echo "clone failed"
+  sleep 2
   exit 1
-)
+}
 
 if [ -z "$extend_modules" ]; then
-  echo "Insert extends modules ? Enter for skip | name dir for accept - using file extend_modules.txt for define"
+  status_modules_clone=()
+  echo "Insert extends modules ? Enter for skip | any key for accept - using file extend_modules.txt for define"
   read -r extend_modules
   if [ -z "$extend_modules" ]; then
     echo "skip insert extend_modules"
   else
-    mkdir -p "$path_install/modules"
-    cd "$path_install/modules" || (echo "find not found path install" && exit 1)
-    while read -r line; do git clone "$line"; done <extend_modules.txt
+    dir_modules="$path_install/modules"
+    if [ -d "$dir_modules" ]; then
+      array_git=()
+
+      echo "'$dir_modules' found and now copying files, please wait ..."
+      while read -r line; do array_git+=("$line"); done <extend_modules.txt
+      cd "$dir_modules" || (echo "not found" && exit 1)
+      for value in "${array_git[@]}"; do
+        git clone "$value"
+      done
+    else
+      echo "Warning: '$dir_modules' NOT found."
+    fi
   fi
 fi
+{
+  cd "$path_install\application" && composer install
+  cd "$path_install\application\config" && cp app-config.php.dist app-config.php
+} || {
+  echo "not found module to run composer"
+}
+
 echo "${arr[*]}"
+echo "${status_modules_clone[*]}"
 read -r -p "Press enter to exit"
